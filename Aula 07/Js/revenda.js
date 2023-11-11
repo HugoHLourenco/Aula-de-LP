0c45e3a8e0911fd305776186b7fa0c357132e514
Hugo H. Lourenço <hugohlourenco@gmail.com>
2023-11-11 13:12:37 -0300

    Function adicionar

--- Aula 07/Js/revenda.js
+++ Aula 07/Js/revenda.js
@@ -1,5 +1,25 @@
 var carros = []
 
-function adicionar(){
-    
+function adicionar() {
+    var inModelo = Number(document.getElementById("inModelo")).value
+    var inPreco = Number(document.getElementById("inPreco")).value
+
+    //  VALIDAR CAMPOS
+    if (inModelo = "" || inPreco == 0 || isNaN(inPreco)) {
+        alert("INFORME CORRETAMENTE OS DADOS!!!")
+        document.getElementById("inModelo").focus()
+    }
+
+    //  ADICIONAR DADOS AO VETOR
+    carros.push({ inModelo: inModelo, inPreco: inPreco })
+
+    //  LIMPA CAMPOS
+    document.getElementById("inModelo").value = ""
+    document.getElementById("inPreco").value = ""
+
+    listar()    //  CHAMAR A FUNÇÃO QUE LISTA OS CARROS
+}
+
+function listar() {
+
 }
