a8ec1fc6063608512f9cfcb7364016026049ce20
Hugo H. Lourenço <hugohlourenco@gmail.com>
2023-11-11 13:39:05 -0300

    Update revenda.js

--- Aula 07/Js/revenda.js
+++ Aula 07/Js/revenda.js
@@ -1,25 +1,41 @@
 var carros = []
 
 function adicionar() {
-    var inModelo = Number(document.getElementById("inModelo")).value
-    var inPreco = Number(document.getElementById("inPreco")).value
+    var inModelo = document.getElementById("inModelo")
+    var inPreco = document.getElementById("inPreco")
+
+    var modelo = inModelo.value
+    var preco = Number(inPreco.value)
 
     //  VALIDAR CAMPOS
-    if (inModelo = "" || inPreco == 0 || isNaN(inPreco)) {
+    if (modelo == "" || preco == 0) {
         alert("INFORME CORRETAMENTE OS DADOS!!!")
         document.getElementById("inModelo").focus()
+    } else {
+        //  ADICIONAR DADOS AO VETOR
+        carros.push({ modelo: modelo, preco: preco })
+    
+        //  LIMPA CAMPOS
+        document.getElementById("inModelo").value = ""
+        document.getElementById("inPreco").value = ""
+    
+        listar()    //  CHAMAR A FUNÇÃO QUE LISTA OS CARROS
     }
 
-    //  ADICIONAR DADOS AO VETOR
-    carros.push({ inModelo: inModelo, inPreco: inPreco })
-
-    //  LIMPA CAMPOS
-    document.getElementById("inModelo").value = ""
-    document.getElementById("inPreco").value = ""
-
-    listar()    //  CHAMAR A FUNÇÃO QUE LISTA OS CARROS
 }
 
 function listar() {
+    if (carros.length == 0) {
+        alert("NÃO HÁ CARROS NA LISTA!!!")
+    }
 
+    var lista = ""
+
+    //  PERCORRE O VETOR    
+    for (let i = 0; i < carros.length; i++) {
+        lista += carros[i].modelo + " - R$ " + carros[i].preco
+    }
+
+    //  PRINTAR NA TELA
+    document.getElementById("outLista").textContent = lista
 }
